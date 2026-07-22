72ae067a0f0869985afa95ae1a3bee3be2481632
mvnpictures56 <mvnpictures56@mvnpictures56ui-MacBookPro.local>
2026-07-22 18:22:16 +0900

    add slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bspringb.github.io',
-	base: '/blowspringbreezeblow',
+	base: '/blowspringbreezeblow/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
